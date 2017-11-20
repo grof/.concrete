6778c92a26be2d3379b432761138500f9fc10d08
Francois Groleau <francois.groleau@gmail.com>
2017-11-19 20:17:32 -0500

    add prompt configuration

---
+++ bash/local/prompt.bash
@@ -0,0 +1,14 @@
+
+if [ -n "$TMUX" ]; then
+  export TERM="screen-256color"
+else
+  export TERM="xterm-256color"
+fi
+
+ATTR_RESET=$(tput sgr0)
+
+PS1_COLOR=$(tput setaf 214)
+PATH_COLOR=$(tput setaf 123)jj
+
+PS1='\[${PS1_COLOR}\]\h>\[${ATTR_RESET}\] '
+
